6acb3d86d08b6b0ae8c1bd41b7b8d6d4ab02e906
Artem <tema.news.as@gmail.com>
2025-11-17 20:20:19 +0300

    ДЗ тема 3

---
+++ ДЗ тема 3.py
@@ -0,0 +1,13 @@
+# Исходные данные
+items = {
+    'milk15': {'name': 'молоко 1.5%', 'count': 34, 'price': 89.9},
+    'cheese': {'name': 'сыр молочный 1 кг.', 'count': 12, 'price': 990.9},
+    'sausage': {'name': 'колбаса 1 кг.', 'count': 122, 'price': 1990.9}
+}
+
+# Формируем новый словарь с помощью dict comprehension:
+# ключ — как в исходной структуре, значение — True, если count < 20, иначе — False
+price_less_20 = {key: (item['count'] < 20) for key, item in items.items()}
+
+# Выводим результат
+print(price_less_20)
